3b29b1358d2d87a8e8e301f6a0b3aa2655a8d159
Mráz Zsolt Dániel <mrazzsolt@gmail.com>
2021-06-02 21:27:22 +0200

    Add project files.

---
+++ diamond_draw/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakorlós
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(print(11));
+            Console.ReadKey();
+        }
+        public static string print(int n)
+        {
+            if (n % 2 == 0 || n < 0)
+            {
+                return null;
+            }
+            string diamond = "";
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    int counter = i;
+                    while (counter != n - 1 && counter < n)
+                    {
+                        if (counter % 2 != 0)
+                        {
+                            diamond += " ";
+                        }
+                        counter++;
+                    }
+                    for (int j = 0; j < i; j++)
+                    {
+                        diamond += "*";
+                    }
+                    diamond += "\n";
+                }
+            }
+            for (int i = n - 1; i >= 1; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    int counter = i;
+                    while (counter != n - 1 && counter < n)
+                    {
+                        if (counter % 2 != 0)
+                        {
+                            diamond += " ";
+                        }
+                        counter++;
+                    }
+                    for (int j = 0; j < i; j++)
+                    {
+                        diamond += "*";
+                    }
+                    diamond += "\n";
+                }
+            }
+            return diamond;
+        }
+    }
+}
